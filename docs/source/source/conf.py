dcee6b8f7388dadc03ea02df1a69f0850dc1e21c
Seasons108 <130933100+alias1947@users.noreply.github.com>
2025-07-11 12:30:58 +0530

    commit draft

--- docs/source/source/conf.py
+++ docs/source/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'QuantumNetworkSim'
+project = 'qkdProtocols'
 copyright = '2025, Jayesh Patil'
 author = 'Jayesh Patil'
 release = '1.0'
@@ -17,12 +17,12 @@ release = '1.0'
 extensions = []
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
--- docs/source/source/index.rst
+++ docs/source/source/index.rst
@@ -1,10 +1,10 @@
-.. QuantumNetworkSim documentation master file, created by
-   sphinx-quickstart on Fri Jul 11 00:10:48 2025.
+.. qkdProtocols documentation master file, created by
+   sphinx-quickstart on Fri Jul 11 01:03:20 2025.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-QuantumNetworkSim documentation
-===============================
+qkdProtocols documentation
+==========================
 
 Add your content using ``reStructuredText`` syntax. See the
 `reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
